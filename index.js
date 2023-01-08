d71aef113356f37f9e1cff7c9f9c611840b22d54
G-nava <navarro9207@gmail.com>
2023-01-07 22:51:19 -0500

    domain changed

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
-const route = (event)=>{
-    event = event || window.event;
-    event.preventDefault();
-    window.history.pushState({}, "", event.target.href);
-    handleLocation();
+// const route = (event)=>{
+//     event = event || window.event;
+//     event.preventDefault();
+//     window.history.pushState({}, "", event.target.href);
+//     handleLocation();
+// };
 
-};
+// const routes = {
+//     404: "src/pages/404.html",
+//     "/": "index.html",
+//     "/nos-casamos": "src/pages/client.html",
+//     // "/client-2":"index.html",
+// };
 
-const routes = {
+// const handleLocation = async()=>{
+//     const path =window.location.pathname
+//     const route = routes[path] || routes[404];
+//     const html = await fetch(route).then((data)=> data.text());
+//     document.getElementById('main-page').innerHTML = html;
+// };
 
-    404:"404.html",
-    "/":"index.html",
-    "/client":"src/pages/client_1/client.html",
-    // "/client-2":"index.html",
-
-};
-
-const handleLocation = async()=>{
-    const path =window.location.pathname
-    const route = routes[path] || routes[404];
-    const html = await fetch(route).then((data)=> data.text());
-    document.getElementById('main-page').innerHTML = html;
-};
-
-window.onpopstate = handleLocation;
-window.addEventListener("locationchange", route())
-
-
-handleLocation();
-
+// window.onpopstate = handleLocation;
+// window.addEventListener("click", route)
 // window.route = route;
+
+// // handleLocation();
