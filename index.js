b5c8bc133d8e7b29814a250bc09638a029c739e0
G-nava <navarro9207@gmail.com>
2023-01-07 21:35:31 -0500

    adding routes

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-function redirect(){
-    window.location = 'src/pages/client_1/index.html';
-}
+const route = (event)=>{
+    event = event || window.event;
+    event.preventDefault();
+    window.history.pushState({}, "", event.target.href);
+    handleLocation();
+
+};
+
+const routes = {
+
+    404:"404.html",
+    "/":"index.html",
+    "/client":"src/pages/client_1/client.html",
+    // "/client-2":"index.html",
+
+};
+
+const handleLocation = async()=>{
+    const path =window.location.pathname
+    const route = routes[path] || routes[404];
+    const html = await fetch(route).then((data)=> data.text());
+    document.getElementById('main-page').innerHTML = html;
+};
+
+window.onpopstate = handleLocation;
+window.addEventListener("locationchange", route())
+
+
+handleLocation();
+
+// window.route = route;
