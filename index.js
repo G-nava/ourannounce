b14d0064c7afa54862f00bdcaa0b0fb3ed9dc4e3
G-nava <navarro9207@gmail.com>
2023-02-05 16:28:20 -0500

    deirections of css changed

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const express = require('express');
+
 const app = express();
 
 const port = 5500;
 
+//motor de plantillas
+app.set('view engine','ejs');
+app.set('views', `${__dirname}/src/views`);
+
+
 app.use(express.static(__dirname + "/public"));
 
-
-app.get('/',(req, res)=>{
-    res.send('mi respuesta desde express')
-})
-
-app.get('/servicio',(req, res)=>{
-    res.send('hi there')
-})
+//rutas web
+app.use('/', require('./src/router/routesWeb'))
 
 app.use((req, res, next)=>{
     res.status(404).sendFile(`${__dirname}/public/404.html`)
--- src/router/routesWeb.js
+++ src/router/routesWeb.js
@@ -1,3 +1,16 @@
+const express = require('express');
+const router = express.Router();
 
+
+router.get('/',(req, res)=>{
+    // res.render(`${__dirname}/public/index.html`,"utf8")
+    res.sendFile(`${__dirname}/public/index.html`,"utf8")
+});
+
+router.get('/servicio',(req, res)=>{
+    res.send('hi there')
+});
+
+module.exports = router;
 
 // app.use(express.static(_dirname + "/public"))
