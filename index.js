0789a9c35b76b3abb17778cee1e18f49d6d0289c
G-nava <navarro9207@gmail.com>
2023-03-30 15:22:59 -0500

    finishing form

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const locationValue = window.location.search;
-    console.log(locationValue);
+    // console.log(locationValue);
     
     //separar datos
 
@@ -15,8 +15,6 @@ const locationValue = window.location.search;
 
 
 /*Intersection observer */
-
-
 const images = document.querySelectorAll('.anim');
 
 observer = new IntersectionObserver((entries) => {
@@ -36,5 +34,37 @@ images.forEach(image =>{
 })
 
 
+/*EXCEL DB */
+
+// const SHEET_ID = "1tRxbmhamPZzTM3vhzsFfcpclvcxygmToIt5mfKahnKE";
+// const SHEET_TITTLE = "data_guests";
+
+// const SHEET_RANGE = ""; // for concatenate :/
+// const URL = `https://docs.google.com/spreadsheets/d/${SHEET_ID}`
+
+// https://docs.google.com/spreadsheets/d/1tRxbmhamPZzTM3vhzsFfcpclvcxygmToIt5mfKahnKE/edit?usp=sharing
+
+// https://sheet.best/api/sheets/3e1004e4-657b-49d3-8a85-a9e84f1f220b
+
+
+// const formulario = document.getElementById('form');
+
+// formulario.addEventListener('submit',(e)=>{
+//     e.preventDefault();  
+
+// });
+
+const form = document.getElementById('form').addEventListener('submit',(e)=>{
+        
+    e.preventDefault();  
+    const guestNumber = document.querySelectorAll('input[type="radio"][name="numPerson"]');
+    const selectedValue = Array.from(guestNumber).find(radio => radio.checked) && Array.from(guestNumber).find(radio => radio.checked).value;;
+    console.log(selectedValue);
+    // guestNumber.forEach((g)=>{
+    // });
+
+        // alert('done');       
+
+})
 
 
